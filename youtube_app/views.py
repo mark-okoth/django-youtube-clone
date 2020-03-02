9cbd71c049ab30da18fd4001ce8693446e0f2e02
mark odhiambo okoth <markokoth96@gmail.com>
2020-03-02 14:08:15 +0300

    about page

--- youtube_app/views.py
+++ youtube_app/views.py
@@ -3,3 +3,9 @@ from django.shortcuts import render
 # Create your views here.
 def home(request):
     return render(request, 'resourses/index.html')
+
+
+
+
+def about(request):
+    return render(request, 'resourses/about.html')
